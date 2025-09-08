88bf4010352f23dae050e0357923436020ab1661
omodele temitope <omodeletemitope12@gmail.com>
2025-09-08 10:10:54 -0700

    initial commit

---
+++ day5.js
@@ -0,0 +1,21 @@
+// 30 DAYS OF JAVASCRIPT
+let challange = "30 days of javascript"
+let challange2 = "Facbook, Google, Microsoft, Apple, IBM, Oracle, Amazon "
+chase = challange.length;
+choose = challange.toUpperCase();
+cook = challange.toLowerCase();
+come = challange.substring(0, 2);
+comb = challange.slice(3, 21);
+collect = challange.includes("script");
+cross = challange.split(" ");
+crop = challange2.split(", ");
+console.log(chase);
+console.log(choose);
+console.log(cook);
+console.log(come);
+console.log(comb);
+console.log(collect)
+console.log(cross)
+console.log(crop)
+
+
